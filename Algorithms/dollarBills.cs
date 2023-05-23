81fdbc589aa87d61a76a4d72b2233449e83615fd
Unekwu001 <tusedor360@gmail.com>
2023-05-23 10:07:53 +0100

    adjusted

--- Algorithms/dollarBills.cs
+++ Algorithms/dollarBills.cs
@@ -13,37 +13,37 @@ namespace Algorithms
 		//ATM machine needs to dispense (in that order).
 		public void Bills(int number)
 		{
-			int countOfHundred;
-			int countOffifty;
-			int countOfTwenty;
+			int countOfHundreds;
+			int countOffiftys;
+			int countOfTwentys;
 
 			if (number % 100 == 0)
 			{
-				countOfHundred = number/100;
-				countOffifty = 0; 
-				countOfTwenty = 0;
-				Console.WriteLine($"{countOfHundred}, {countOffifty}, {countOfTwenty}");
+				countOfHundreds = number/100;
+				countOffiftys = 0; 
+				countOfTwentys = 0;
+				Console.WriteLine($"{countOfHundreds}, {countOffiftys}, {countOfTwentys}");
 			}
 			else if (number % 50 == 0) 
 			{
-				countOfHundred = (number - 50)/100;
-				countOffifty = 1;
-				countOfTwenty = 0;
-				Console.WriteLine($"{countOfHundred}, {countOffifty}, {countOfTwenty}" );
+				countOfHundreds = (number - 50)/100;
+				countOffiftys = 1;
+				countOfTwentys = 0;
+				Console.WriteLine($"{countOfHundreds}, {countOffiftys}, {countOfTwentys}" );
 			}
 			else if (number % 20 == 0)
 			{
-				countOfHundred = number / 100;
-				countOffifty = 0;
-				countOfTwenty = 1;
-				Console.WriteLine($"{countOfHundred}, {countOffifty}, {countOfTwenty}");
+				countOfHundreds = number / 100;
+				countOffiftys = 0;
+				countOfTwentys = 1;
+				Console.WriteLine($"{countOfHundreds}, {countOffiftys}, {countOfTwentys}");
 			}
 			else
 			{
-				countOfHundred = ((number+50)/ 100)-1;
-				countOffifty = 1;
-				countOfTwenty = ((number+50)%100)/20;
-				Console.WriteLine($"{countOfHundred}, {countOffifty}, {countOfTwenty}");
+				countOfHundreds = ((number+50)/ 100)-1;
+				countOffiftys = 1;
+				countOfTwentys = ((number+50)%100)/20;
+				Console.WriteLine($"{countOfHundreds}, {countOffiftys}, {countOfTwentys}");
 
 			}
 			 
